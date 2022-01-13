2a60db2e8ccc271c97b505bef569a90df65fdbd1
ntym4ek <mega_comp@mail.ru>
2022-01-13 11:38:10 +0300

    иконки и манифест

--- sites/all/themes/vctheme/templates/system/html.tpl.php
+++ sites/all/themes/vctheme/templates/system/html.tpl.php
@@ -2,15 +2,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head>
-  <link rel="profile" href="<?php print $grddl_profile; ?>" />
-<!--  <link rel="manifest" href="/manifest.webmanifest"> установка не нужна -->
-
-  <meta name="msapplication-TileImage" content="/sites/default/files/logo/192.png">
-  <meta name="msapplication-TileColor" content="#2F3BA2">
-
-  <link rel="apple-touch-icon" href="/sites/default/files/logo/192.png">
-  <meta name="theme-color" content="#213a6e"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="profile" href="<?php print $grddl_profile; ?>" />
+
+  <link rel="manifest" href="/manifest.ru.webmanifest">
+  <meta name="msapplication-TileImage" content="/sites/default/files/logo/icon-192x192.png">
+  <meta name="msapplication-TileColor" content="#2F3BA2">
+  <link rel="apple-touch-icon" href="/sites/default/files/logo/icon-192x192.png">
+
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
